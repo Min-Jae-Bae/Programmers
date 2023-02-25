43dce1d4d457d4d00c8ea6fde821d000f4d14beb
Min-Jae-Bae <qoalswo123@icloud.com>
2023-02-25 20:18:44 +0900

    feat: Odd and Even

--- app/src/main/java/com/example/programmers/Solution.kt
+++ app/src/main/java/com/example/programmers/Solution.kt
@@ -1,12 +1,10 @@
 package com.example.programmers
 
-class Solution {
-    fun solution(num: Int): String {
-        var answer = ""
+fun oddAndEven(num: Int): String {
+    var answer = ""
 
-        if (num % 2 == 0) answer = "Even"
-        if (num % 2 != 0) answer = "Odd"
+    if (num % 2 == 0) answer = "Even"
+    if (num % 2 != 0) answer = "Odd"
 
-        return answer
-    }
+    return answer
 }
