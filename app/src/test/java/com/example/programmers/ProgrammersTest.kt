6de04cded8e4201b03dc20074b08a8b70a244bef
Min-Jae-Bae <qoalswo123@icloud.com>
2023-02-26 14:00:31 +0900

    test: Array average return
    Use MethodSource annotation

--- app/src/test/java/com/example/programmers/ProgrammersTest.kt
+++ app/src/test/java/com/example/programmers/ProgrammersTest.kt
@@ -3,7 +3,9 @@ package com.example.programmers
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
 
 class ProgrammersTest {
 
@@ -12,8 +14,21 @@ class ProgrammersTest {
         "3, Odd",
         "4, Even"
     )
-    fun `Check odd or even`(number: Int, oddOrEven: String) {
+    fun `Correct odd even check test`(number: Int, oddOrEven: String) {
         assertThat(checkOddOrEven(number = number), `is`(oddOrEven))
     }
 
+    @ParameterizedTest
+    @MethodSource("case")
+    fun `Array average return test`(arr: IntArray, mean: Double) {
+        assertThat(createMean(arr = arr), `is`(mean))
+    }
+
+    companion object {
+        @JvmStatic
+        fun case() = listOf(
+            Arguments.of(arrayListOf(1,2,3,4), 2.5),
+            Arguments.of(arrayListOf(5, 5), 5)
+        )
+    }
 }
