9e50a211b839f5997c344d9b684b6333d7686073
Min-Jae-Bae <qoalswo123@icloud.com>
2023-03-20 23:50:37 +0900

    feat: yin yang add
    Use mapIndexed

--- app/src/main/java/com/example/programmers/Solution2.kt
+++ app/src/main/java/com/example/programmers/Solution2.kt
@@ -1,7 +1,11 @@
 package com.example.programmers
 
-fun hidePhoneNumber(phoneNumber: String): String
-    = phoneNumber
-        .take(phoneNumber.length - 4)
-        .map { "*" }
-        .joinToString(separator = "", postfix = phoneNumber.takeLast(4))
+fun hidePhoneNumber(phoneNumber: String): String = phoneNumber
+    .take(phoneNumber.length - 4)
+    .map { "*" }
+    .joinToString(separator = "", postfix = phoneNumber.takeLast(4))
+
+fun plusYinYang(absoluteValues: IntArray, signs: BooleanArray): Int =
+    signs.mapIndexed { index, hasPlusSign ->
+        if (hasPlusSign) absoluteValues[index] else -absoluteValues[index]
+    }.sum()
