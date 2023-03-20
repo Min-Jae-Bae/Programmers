3efb3905e31880a38e3936858b4666d43af62631
Min-Jae-Bae <qoalswo123@icloud.com>
2023-03-20 19:00:09 +0900

    test: yin yang addition test

--- app/src/test/java/com/example/programmers/ProgrammersTest2.kt
+++ app/src/test/java/com/example/programmers/ProgrammersTest2.kt
@@ -3,7 +3,9 @@ package com.example.programmers
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
 
 class ProgrammersTest2 {
     @ParameterizedTest
@@ -13,5 +15,27 @@ class ProgrammersTest2 {
     )
     fun `Hide the last 4 digits of your phone number`(phoneNumber: String, hidedNumber: String) {
         assertThat(hidePhoneNumber(phoneNumber = phoneNumber), `is`(hidedNumber))
+    }
+
+    @ParameterizedTest
+    @MethodSource("getYinYangCase")
+    fun `Yin yang addition test`(
+        absoluteValues: IntArray,
+        signs: BooleanArray,
+        additionResult: Int,
+    ) {
+        assertThat(
+            plusYinYang(absoluteValues = absoluteValues, signs = signs),
+            `is`(additionResult)
+        )
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun getYinYangCase() = listOf(
+            Arguments.of(intArrayOf(4, 7, 12), booleanArrayOf(true, false, true), 9),
+            Arguments.of(intArrayOf(1, 2, 3), booleanArrayOf(false, false, true), 0)
+        )
     }
 }
